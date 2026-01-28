146818ebc4848a6d5b69852f847b8fe48c394173
c-g <no-email>
2026-01-28 09:54:45 +0000

    feat: add index and about pages

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+	about_relative_url = reverse('rango:about')
+	about_absolute_url = request.build_absolute_uri(about_relative_url)
+	return HttpResponse('Rango says hey there partner! About page: ' + about_absolute_url + '.')
+
+def about(request):
+	index_relative_url = reverse('rango:index')
+	index_absolute_url = request.build_absolute_uri(index_relative_url)
+	return HttpResponse('Rango says here is the about page. Index page: ' + index_absolute_url + '.')
