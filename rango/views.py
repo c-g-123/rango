e687e600e37496d18d8c125db4195eb14b1616ff
c-g <no-email>
2026-01-29 14:34:53 +0000

    feat: chapter 3 tests pass

--- rango/views.py
+++ rango/views.py
@@ -6,9 +6,9 @@ from django.urls import reverse
 def index(request):
 	about_relative_url = reverse('rango:about')
 	about_absolute_url = request.build_absolute_uri(about_relative_url)
-	return HttpResponse('Rango says hey there partner! <a href="' + about_absolute_url + '">About</a>.')
+	return HttpResponse('Rango says hey there partner! <a href="' + about_relative_url + '">About</a>.')
 
 def about(request):
 	index_relative_url = reverse('rango:index')
 	index_absolute_url = request.build_absolute_uri(index_relative_url)
-	return HttpResponse('Rango says here is the about page. <a href="' + index_absolute_url + '">Index</a>.')
+	return HttpResponse('Rango says here is the about page. <a href="' + index_relative_url + '">Index</a>.')
